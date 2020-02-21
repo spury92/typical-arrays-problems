5f0cd26eb287d4af75da7355ddaa593d15edd08e
spury92 <bot1412@yandex.ru>
2020-02-22 01:33:37 +0300

    pass all 63 tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    return Math.min(...array)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    return Math.max(...array)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+  }
 }
